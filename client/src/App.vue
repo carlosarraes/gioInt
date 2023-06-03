8fde28234c3d043960284c5b8176aea66591db0d
carraes <carraeshb@gmail.com>
2023-06-02 23:46:36 -0300

    feat: setup routing, env api key

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello</h1>
+  <router-view />
 </template>
 
 <style scoped></style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,24 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp, ref } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './style.css'
+import App from './App.vue'
+import Login from './pages/Login.vue'
+import Success from './pages/Success.vue'
 
-const app = createApp(App);
+const routes = [
+  { path: '/', component: Login, name: 'login' },
+  { path: '/success', component: Success, name: 'success' },
+]
 
-app.mount("#app");
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+const hankoApi = ref(import.meta.env.VITE_HANKO_API_URL)
+
+const app = createApp(App)
+
+app.provide('hankoApi', hankoApi)
+app.use(router)
+app.mount('#app')
